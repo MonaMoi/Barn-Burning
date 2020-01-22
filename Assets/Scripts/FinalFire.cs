d76ff81b34b073c8bbd5522945769c4a9a403c54
marquitoh <markus.haberl95@googlemail.com>
2020-01-22 18:25:43 +0100

    FinalFire
    Trigger Script + Sound

---
+++ Assets/Scripts/FinalFire.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalFire : MonoBehaviour
+{
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+    public Transform Spawnpoint1;
+    public GameObject Prefab1;
+    public Transform Spawnpoint2;
+    public GameObject Prefab2;
+    public Transform Spawnpoint3;
+    public GameObject Prefab3;
+    public Transform Spawnpoint4;
+    public GameObject Prefab4;
+    public Transform Spawnpoint5;
+    public GameObject Prefab5;
+    public Transform Spawnpoint6;
+    public GameObject Prefab6;
+    public Transform Spawnpoint7;
+    public GameObject Prefab7;
+    public Transform Spawnpoint8;
+    public GameObject Prefab8;
+    public Transform Spawnpoint9;
+    public GameObject Prefab9;
+    public Transform Spawnpoint10;
+    public GameObject Prefab10;
+    public Transform Spawnpoint11;
+    public GameObject Prefab11;
+
+    void OnTriggerEnter (Collider other)
+    {
+        Debug.Log ("Object Entered the trigger");
+        Debug.Log("Object is within trigger");
+        Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+        Instantiate(Prefab1, Spawnpoint1.position, Spawnpoint1.rotation);
+        Instantiate(Prefab2, Spawnpoint2.position, Spawnpoint2.rotation);
+        Instantiate(Prefab3, Spawnpoint3.position, Spawnpoint3.rotation);
+        Instantiate(Prefab4, Spawnpoint4.position, Spawnpoint4.rotation);
+        Instantiate(Prefab5, Spawnpoint5.position, Spawnpoint5.rotation);
+        Instantiate(Prefab6, Spawnpoint6.position, Spawnpoint6.rotation);
+        Instantiate(Prefab7, Spawnpoint7.position, Spawnpoint7.rotation);
+        Instantiate(Prefab8, Spawnpoint8.position, Spawnpoint8.rotation);
+        Instantiate(Prefab9, Spawnpoint9.position, Spawnpoint9.rotation);
+        Instantiate(Prefab10, Spawnpoint10.position, Spawnpoint10.rotation);
+       Instantiate(Prefab11, Spawnpoint11.position, Spawnpoint11.rotation);
+
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+         }
+
+    void OnTriggerExit (Collider other)
+    {
+        Debug.Log("Object Exit the trigger");
+    }
+}
