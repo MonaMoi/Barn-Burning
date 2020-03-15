204b58c882e2702d1b303143de7e8d90f9412e2d
marquitoh <markus.haberl95@googlemail.com>
2020-03-15 20:22:44 +0100

    FingerSnap Sound, BenGameSound, Animation Versuch, Musik im Menue

---
+++ Assets/Scripts/AnimationControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+
+    public Animator anim;
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine("Wait");
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3.0f);
+        anim.Play("Verschwinden");
+    }
+}
